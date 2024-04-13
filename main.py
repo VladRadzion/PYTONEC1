ec8f49f946dca5e5b525f4ae72262097b0d24860
C2122 <d>
2024-04-13 07:30:08 -0700

    Change all code

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
 
-from turtle import *
-color('green')
-pensize(10)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
+class Student:
+    def __init__(self,name = 'nonana'):
+        self.name = name
+
+    def __int__(self):
+        return f'hello, my neme is {self.name}'
+    def __del__(self):
+        print('Im granduater))')
+
+stud= Student ('Andriy')
+print(stud)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Student:
+#     amount_of_students = 0
+#     def __init__(self,height=0, name='noname',mark=0):
+#         self.height = height
+#         self.name = name
+#         self.mark = mark
+#         print('ЗРІССТСССТТТ',self.name,':',self.height)
+#         Student.amount_of_students += 1
+#     def growUp(self):
+#             self.height += 10000000000000000000000
+#     def growmark(self):
+#             self.mark += 1
+#
+#
+#
+# maxym = Student(height=10000000000000000000001000000000000000000000000000000001)
+# maria = Student()
+#
+# print(Student.amount_of_students)
+# print('мАКСА ЗРІСТ',maxym.height)
+# print('мАРІЇ ЗРІСТ',maria.height)
+#
+# print(maxym.mark)
+# maxym.growmark()
+# print(maxym.mark)
+
